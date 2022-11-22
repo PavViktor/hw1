ae25024b3d29059f1cbefc78bcc17608ab06a619
vsf.viktor <vsf.viktor@gmail.com>
2022-11-22 13:32:46 +0300

    task 1 done

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,19 +1,18 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-
 // a > 5; b < 7 -> max = 7; a > 2 b < 10 -> max = 10; a > -9 b < -3 -> max = -
 
-
 Console.WriteLine("Ether the number:");
-int num1 = int.Parse(Console.WriteLine());
+int num1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Ether the number:");
-int num2 = int.Parse(Console.WriteLine());
-
+int num2 = int.Parse(Console.ReadLine());
 
 if(num1 > num2)
 {
-    Console.WriteLine($"Число {num1} больше числа {num2}");
+    Console.WriteLine($"the number {num1} greater than the number {num2}");
+    Console.WriteLine($"max = {num1}");
 }
 else
 {
-    Console.WriteLine($"Число {num1} меньше числа {num2}");
+    Console.WriteLine($"the number {num1} is less than the number {num2}");
+    Console.WriteLine($"max = {num2}");
 }
