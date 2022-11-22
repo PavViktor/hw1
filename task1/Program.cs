d57637c9a95ca8cc80c889bd6810b2bbc3dcc4c1
vsf.viktor <vsf.viktor@gmail.com>
2022-11-22 13:20:22 +0300

    third

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a > 5; b < 7 -> max = 7; a > 2 b < 10 -> max = 10; a > -9 b < -3 -> max = -
+
+
+Console.WriteLine("Ether the number:");
+int num1 = int.Parse(Console.WriteLine());
+Console.WriteLine("Ether the number:");
+int num2 = int.Parse(Console.WriteLine());
+
+
+if(num1 > num2)
+{
+    Console.WriteLine($"Число {num1} больше числа {num2}");
+}
+else
+{
+    Console.WriteLine($"Число {num1} меньше числа {num2}");
+}
