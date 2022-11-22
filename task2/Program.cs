acb1246b0ba88ee7a789d1e0cf7deffdd87aedaa
vsf.viktor <vsf.viktor@gmail.com>
2022-11-22 14:46:49 +0300

    task 3 done

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7; 44 5 78 -> 78; 22 3 9 -> 22
+
+Console.WriteLine("Ether the number:");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ether the number:");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ether the number:");
+int num3 = int.Parse(Console.ReadLine());
 
